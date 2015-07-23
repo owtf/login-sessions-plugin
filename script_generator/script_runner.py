b76edde46585375f50740e48c75d91d9e045738d
delta24 <viyat001@gmail.com>
2015-07-23 19:43:43 +0530

    adding basic logging

--- script_generator/script_runner.py
+++ script_generator/script_runner.py
@@ -5,18 +5,43 @@ import base64
 import uuid
 import json
 import re
+import logging
+
 from selenium import webdriver
 
-
-ROOT_DIR = os.path.dirname(sys.argv[0])
+######## DIR SETUP #############################
+ROOT_DIR = os.path.abspath(sys.argv[0])
 LOG_DIR = os.path.abspath(os.path.join(ROOT_DIR, '..', '/logs/'))
 SCRIPT_DIR = os.path.abspath(os.path.join(ROOT_DIR, '..', '/scripts/'))
+###############################################
+
+# basic logging setup
+logger = logging.getLogger("script generator")
+logger.setLevel(logging.ERROR)
+
+#filehandler
+#filelog = logging.FileHandler(log_path)
+#filelog.setLevel(logging.DEBUG)
+
+# console logging
+console = logging.StreamHandler()
+console.setLevel(logging.ERROR)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#filelog.setFormatter(formatter)
+console.setFormatter(formatter)
+
+# add the handlers to the logger
+#logger.addHandler(filelog)
+logger.addHandler(console)
+
+###############################################
 
 # get the conf options for PhantomJS
 with open("conf.json", "r") as config:
     conf = json.load(config)
 
-#print(conf)
 
 # get a UUID - URL safe, Base64
 def get_uuid():
@@ -44,23 +69,26 @@ class LoginScript(object):
         if script:
             self.script = script_name
         else:
-            print("No script specified! Exiting....")
+            logger.error("No script specified! Exiting....")
             sys.exit(-1)
         self.check_pattern = conf["check_pattern"]
         self.browser = self.prep()
         self.id = get_uuid()
 
     def prep(self):
-        return webdriver.PhantomJS(
-            service_args=[
-                '--ignore-ssl-errors=true',
-                '--proxy='+conf["proxy"],
-                '--proxy-type=http',
-                '--disk-cache=true',
-                '--debug='+conf["debug"]
-            ],
-            service_log_path=LOG_DIR
-        )
+        try:
+            return webdriver.PhantomJS(
+                        service_args=[
+                            '--ignore-ssl-errors=true',
+                            '--proxy='+conf["proxy"],
+                            '--proxy-type=http',
+                            '--disk-cache=true',
+                            '--debug='+conf["debug"]
+                        ],
+                        service_log_path=LOG_DIR
+            )
+        except WebDriverException:
+            logger.critical("Cannot load the browser instance")
 
     def run(self):
         with open(os.path.join(SCRIPT_DIR, script_name), "r") as file:
@@ -77,6 +105,15 @@ class LoginScript(object):
         match = re.search(self.check_pattern, source)
         if match:
             print(STATUSES["success"])
+            logger.success(STATUSES["success"])
         else:
             print(STATUSES["failure"])
+            logger.warn(STATUSES["failure"])
 
+    def teardown(self):
+        """
+        cleanup process after the login script is played
+
+        """
+        # first close the driver
+        self.browser.quit()
